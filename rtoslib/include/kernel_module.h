2e13de753b1a0e02f72182ee3074a5378b453c64
graudtV <55806873+graudtV@users.noreply.github.com>
2021-03-16 00:29:16 +0300

    extract task functionallity into Task class
    rtosconfig update will be in the next commit

--- rtoslib/include/kernel_module.h
+++ rtoslib/include/kernel_module.h
@@ -7,40 +7,59 @@ namespace rt {
 
 using PfnTaskRouting = void (*) (void);
 
+class Task final {
+public:
+	/* User API for managing tasks */
+	void suspend() { m_is_suspended = true; }
+	void resume() { m_is_suspended = false; }
+	void abort() { m_is_suspended = m_start_from_begining = true; }
+
+	bool is_suspended() const { return m_is_suspended; }
+	bool is_aborted() const { return m_is_suspended == true && m_start_from_begining == true; }
+
+private:
+	PfnTaskRouting m_routine_address = nullptr;
+	task_stack_sz_t m_stack_size = 0;
+	addr_t m_stack_pointer = 0;
+	addr_t m_stack_starting = 0;
+	bool m_start_from_begining = true;
+	bool m_is_suspended = false;
+
+	friend class Kernel;
+
+	/* Library will create tasks itself, you do not need to create them manually */
+	Task(PfnTaskRouting routine_address, task_stack_sz_t stack_size) :
+		m_routine_address(routine_address),
+		m_stack_size(stack_size) {}
+};
+
 class Kernel final {
 public:
-	static Kernel& get_instance() { static Kernel only_one; return only_one; }
+	static constexpr Kernel& get_instance() { return m_only_one; }
 
 	void run();
 	void relinquish();
-	void suspend() { suspend_task(m_current_task); }
+	void suspend() { current_task().suspend(); }
 
-	void suspend_task(task_id_t task_id) { m_task_is_suspended[task_id] = true; }
-	void resume_task(task_id_t task_id) { m_task_is_suspended[task_id] = false; }
-	void abort_task(task_id_t task_id) { m_task_is_suspended[task_id] = m_task_start_from_begining[task_id] = true; }
-	// void restart_task(task_id_t task_id) {}
-	// void reset_task(task_id_t task_id);
-	/* ... */
+	task_id_t ntasks() const { return m_ntasks; }
+	Task& task(task_id_t task_id) { return m_tasks[task_id]; }
+	Task& current_task() { return m_tasks[m_current_task]; }
+
 private:
-	Kernel();
+	Kernel() {}
 	~Kernel() {}
 	Kernel(const Kernel& other) = delete;
 	Kernel(Kernel&& other) = delete;
 	Kernel& operator =(const Kernel& other) = delete;
 	Kernel& operator =(Kernel&& other) = delete;
 
-	static const int m_ntasks;
-	static const PfnTaskRouting m_task_routine_addresses[];
-	static task_stack_sz_t m_task_stack_sizes[];
-	static addr_t m_task_stack_pointers[];
-	static addr_t m_task_stack_starting[];
-	static bool m_task_start_from_begining[];
-	static bool m_task_is_suspended[];
-
+	static Kernel m_only_one;
+	static const task_id_t m_ntasks;
+	static Task m_tasks[];
 	static task_id_t m_current_task;
 };
 
-inline Kernel& kernel = Kernel::get_instance();
+inline constexpr Kernel& kernel = Kernel::get_instance();
 
 } // rt namespace end
 
--- rtoslib/src/kernel_module.cpp
+++ rtoslib/src/kernel_module.cpp
@@ -1,47 +1,49 @@
 #include <Mipt-RTOS/kernel_module.h>
 #include <Mipt-RTOS/asm_macro.h>
 
+#include <iostream>
+
 namespace rt {
 
-Kernel::Kernel()
+void Kernel::run()
 {
+	/*  Declaring task_id before sp variable (i.e. outside loop) in order not
+	 * to change sp after it was acquired. Actually, it shouldn't influence anything,
+	 * but in such way it seems more explicit */
 	task_id_t task_id;
 	addr_t sp = asm_get_stack_pointer();
 
 	for (task_id = 0; task_id < m_ntasks; ++task_id) {
-		m_task_stack_starting[task_id] = sp;
-		sp -= m_task_stack_sizes[task_id];
-		/* m_task_start_from_begining[task_id] = true automatically */
-		/* m_task_is_suspended[task_id] = false automatically */
+		m_tasks[task_id].m_stack_starting = sp;
+		sp -= m_tasks[task_id].m_stack_size;
+		/* m_tasks[task_id].m_start_from_begining = true automatically */
+		/* m_tasks[task_id].m_is_suspended = false automatically */
 	}
-}
 
-void Kernel::run()
-{
-	m_task_start_from_begining[0] = false;
 	m_current_task = 0;
-	m_task_routine_addresses[0] ();
-	abort_task(0);
+	m_tasks[0].m_start_from_begining = false;
+	m_tasks[0].m_routine_address();
+	m_tasks[0].abort();
 	relinquish(); // TODO: think if it is OK
 }
 
 void Kernel::relinquish()
 {
 	asm_pushall();
-	m_task_stack_pointers[m_current_task] = asm_get_stack_pointer();
+	current_task().m_stack_pointer = asm_get_stack_pointer();
 
 	while (1) {
 		do {
 			if (++m_current_task == m_ntasks)
 				m_current_task = 0;
-		} while (m_task_is_suspended[m_current_task] == true);
-		if (m_task_start_from_begining[m_current_task] == true) {
-			m_task_start_from_begining[m_current_task] = false;
-			asm_set_stack_pointer(m_task_stack_starting[m_current_task]);
-			m_task_routine_addresses[m_current_task] ();
-			abort_task(m_current_task);
+		} while (current_task().m_is_suspended == true);
+		if (current_task().m_start_from_begining == true) {
+			current_task().m_start_from_begining = false;
+			asm_set_stack_pointer(current_task().m_stack_starting);
+			current_task().m_routine_address();
+			current_task().abort();
 		} else {
-			asm_set_stack_pointer(m_task_stack_pointers[m_current_task]);
+			asm_set_stack_pointer(current_task().m_stack_pointer);
 			asm_popall();
 			return;
 		}
