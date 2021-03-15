9177b2cfa5196f2a429e90e0c425eeefb564e04f
graudtV <55806873+graudtV@users.noreply.github.com>
2021-03-16 00:20:22 +0300

    critical buf fixes with asm

--- rtoslib/include/asm_macro.h
+++ rtoslib/include/asm_macro.h
@@ -10,25 +10,30 @@
 			"push %rbx\n\t"			\
 			"push %rcx\n\t"			\
 			"push %rdx\n\t"			\
+			"push %rsi\n\t"			\
+			"push %rdi\n\t"			\
 		);
+
 
 	#define asm_popall()			\
 		asm volatile (				\
+			"pop %rdi\n\t"			\
+			"pop %rsi\n\t"			\
 			"pop %rdx\n\t"			\
 			"pop %rcx\n\t"			\
 			"pop %rbx\n\t"			\
 			"pop %rax\n\t"			\
 		);
 
-	#define asm_get_stack_pointer()					\
-		({											\
-			addr_t _sp = 0;							\
-			asm ("movq %%rsp, %0" : "=r"(_sp) : );	\
-			_sp;									\
+	#define asm_get_stack_pointer()							\
+		({													\
+			addr_t _sp = 0;									\
+			asm volatile ("movq %%rsp, %0" : "=r"(_sp) : );	\
+			_sp;											\
 		})
 
-	#define asm_set_stack_pointer(sp)				\
-		asm ("movq %0, %%rsp" : : "r"((sp)))
+	#define asm_set_stack_pointer(sp)	\
+		asm volatile ("movq %0, %%rsp" : : "r"((sp)) : "rsp")
 #else
 	static_assert(0, SUPPORTED_PLATFORMS);
 #endif
